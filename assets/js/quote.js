555abad758665db555f607ae578857c5e31fc021
cakeinmilk <barrydonovan@gmail.com>
2023-02-15 00:02:20 +0000

    Update quote.js

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -2,8 +2,14 @@ function textOfToday() {
 
   var textOfTodayArray =
   ['No naughty hands allowed.',
-  'String 2', 
-  '#bazzing it 24/7',
+  'Trust the algorithm', 
+  '#bazzing 24/7',
+  'A Peloton Subsidiary',
+  'just poker (no axe throwing)',
+  'spider plants improve air quality',
+  'are you here because pokerstars needs updating?',
+  'but where are we playing poker?',
+  'brb honking on a crack pipe'
   ];
 
   document.getElementById('quoteDisplay').innerHTML = textOfTodayArray[Math.floor(Math.random() * textOfTodayArray.length)];
